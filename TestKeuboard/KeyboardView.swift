05a701b4f77357725b0b9cc12281b266f6cf138d
Albilaga <albilaga.lp@gmail.com>
2020-09-10 02:41:51 +0700

    Add Change Mode

--- TestKeuboard/KeyboardView.swift
+++ TestKeuboard/KeyboardView.swift
@@ -12,7 +12,26 @@ struct KeyboardView: View {
     let controller : UIInputViewController
     @State var text=""
     @State var isTextActive: Bool? = false
+    
+    @State var isExtendedMode = false
+    
     var body: some View {
+        keyboardView
+    }
+    
+    func keyboardAction(_ text:String) -> Void {
+        if isTextActive == true {
+            self.text += text
+        } else {
+           controller.textDocumentProxy.insertText(text)
+        }
+    }
+    
+    
+}
+
+private extension KeyboardView {
+    func normalView() -> some View {
         VStack{
             HStack{
                 Button(action: {
@@ -41,16 +60,66 @@ struct KeyboardView: View {
                 }
             }
             CustomTextField(text: $text, nextResponder: .constant(nil), isResponder: $isTextActive, isSecured: false, keyboard: .default).frame(width: 360, height: 40.0).background(/*@START_MENU_TOKEN@*/Color.orange/*@END_MENU_TOKEN@*/)
+            
+            Button(action: {
+                self.changeMode()
+            }){ Text("Change Mode")
+            }
         }
     }
     
-    func keyboardAction(_ text:String) -> Void {
-        if isTextActive == true {
-            self.text += text
-        } else {
-           controller.textDocumentProxy.insertText(text)
+    func extendedView() -> some View {
+        VStack{
+            HStack{
+                Button(action: {
+                    self.keyboardAction("Q")
+                }) {Text("Q")
+                }
+                Button(action: {
+                    self.keyboardAction("W")
+                }) {Text("W")
+                }
+                Button(action: {
+                    self.keyboardAction("E")
+                }) {Text("E")
+                }
+                Button(action: {
+                    self.keyboardAction("R")
+                }) {Text("R")
+                }
+                Button(action: {
+                    self.keyboardAction("T")
+                }) {Text("T")
+                }
+                Button(action: {
+                    self.keyboardAction("Y")
+                }) {Text("Y")
+                }
+            }
+            CustomTextField(text: $text, nextResponder: .constant(nil), isResponder: $isTextActive, isSecured: false, keyboard: .default).frame(width: 360, height: 40.0).background(/*@START_MENU_TOKEN@*/Color.orange/*@END_MENU_TOKEN@*/)
+            
+            Button(action: {
+                self.changeMode()
+            }){ Text("Change Mode")
+            }
+            
+            Text("Another Text")
+            Text("Another Text #2")
         }
     }
+    
+    func changeMode() {
+        self.isExtendedMode = !self.isExtendedMode
+    }
+    
+    var keyboardView: AnyView{
+        if self.isExtendedMode {
+            return AnyView(extendedView())
+        } else {
+            return AnyView(normalView())
+        }
+    }
+    
 }
 
 struct KeyboardView_Previews: PreviewProvider {
